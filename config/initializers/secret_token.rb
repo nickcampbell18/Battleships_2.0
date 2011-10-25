255f62086114b6cf4f27d03ac02a37c2bf90ed9c
Nick Campbell <nickcampbell18@gmail.com>
2011-10-25 19:37:22 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battleships::Application.config.secret_token = '256c789170901da7d47c0f9c5908a4b48f35982850fd5193d0f853080200eb000d22aeb4e13a189b708d71ac7cdd0444998c35af3e74683de281ff5b09ad2130'
